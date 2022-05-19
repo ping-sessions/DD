74d5fd2a087aabd59b53eb3e1dea3356a0ac3ec3
saschakrischock <s.krischock@gmail.com>
2022-05-19 19:08:43 +0200

    clip

--- site/snippets/file.php
+++ site/snippets/file.php
@@ -1,5 +1,5 @@
 <?php $siteurl = $site->url()?>
 
-<div class="projects__item">
+<div class="projects__item clip1">
   <a href = '<?= $siteurl."/home/projects/".$fileObj->project ?>'><img src = '<?= $fileObj->url ?>' /></a>
 </div>
