3dfdb092ee9e6c9a9990bf2ccf449fd7364788fe
nkry <nickr2093@gmail.com>
2022-06-07 19:30:29 +0200

    image optimization + lazyload

--- site/snippets/file.php
+++ site/snippets/file.php
@@ -1,9 +1,9 @@
 <?php $siteurl = $site->url()?>
 
 <div data-bg="#fff" data-tags='<?= implode(', ', $fileObj->tags) ?>' data-number="(<?= $fileObj->position + 1 ?>)" data-title="<?= $fileObj->project_title ?>" class="projects__item  <?php if ($fileObj->type == 'image') : ?><?php endif ?>">
-  <a    <?php if ($fileObj->type != 'image') : ?>  class="circ-grad"   <?php endif ?> data-position="<?= $fileObj->position ?>" href = '<?= $siteurl."/home/projects/".$fileObj->project ?>'>
+  <a <?php if ($fileObj->type != 'image') : ?>  class="circ-grad"   <?php endif ?> data-position="<?= $fileObj->position ?>" href = '<?= $siteurl."/home/projects/".$fileObj->project ?>'>
     <?php if ($fileObj->type == 'image') : ?>
-      <img src = '<?= $fileObj->url ?>' />
+      <img class='lazy' data-src='<?= $fileObj->resize_url ?>' />
     <?php elseif ($fileObj->type == 'audio') : ?>
       <audio controls>
         <source src="<?= $fileObj->url?>" type="audio/mpeg">
--- site/templates/text.php
+++ site/templates/text.php
@@ -1,5 +1,3 @@
 <?php snippet('header') ?>
 
-<div>TEXT PAGE</div>
-
 <?php snippet('footer') ?>
